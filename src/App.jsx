1c3b234762336e16d24100f297f4cfd808faa7ce
Ayushi Patel <aayushi.1831@gmail.com>
2024-11-23 16:00:16 -0600

    Add component to fetch yoga data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
 
 function App() {
   return (
- <>
- <FetchData/>
- </>
+    <>
+      <h1>Fetch Data</h1>
+      <FetchData/>
+      <h1> Fetch Yoga Data</h1>
+      <FetchYogaData/>
+    </>
   )
 }
 
